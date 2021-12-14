79dd828c41ff7829edef7d590cc24b706f06a04f
aljohnvillacruel141@gmail.com <aljohnvillacruel141@gmail.com>
2021-12-14 20:32:41 +0800

    finish part 8

--- catalog/urls.py
+++ catalog/urls.py
@@ -12,7 +12,10 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('authors/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
 ]
 urlpatterns += [
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path(r'borrowed/', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Book, Author, BookInstance, Genre
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin
 
 @login_required
 def index(request):
@@ -17,7 +18,7 @@ def index(request):
 
     num_visits = request.session.get('num_visits', 0)
     request.session['num_visits'] = num_visits + 1
-
+    all_genre = Genre.objects.all().count()
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
@@ -27,6 +28,7 @@ def index(request):
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
         'num_visits': num_visits,
+        'all_genre': all_genre,
     }
 
     # Render the HTML template index.html with the data in the context variable
@@ -41,8 +43,7 @@ from django.views import generic
 
 class BookListView(LoginRequiredMixin,generic.ListView):
     model = Book
-    paginate_by = 1
-
+    paginate_by = 2
 class BookDetailView(LoginRequiredMixin,generic.DetailView):
     model = Book
 
@@ -55,3 +56,30 @@ class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
 
     def get_queryset(self):
         return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    
+class AuthorListView(generic.ListView):
+    model = Author
+
+
+# part - 8 
+class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
+    """Generic class-based view listing books on loan to current user."""
+    model = BookInstance
+    template_name ='catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+    
+class LoanedBooksAllListView(PermissionRequiredMixin, generic.ListView):
+    """Generic class-based view listing all books on loan. Only visible to users with can_mark_returned permission."""
+    model = BookInstance
+    permission_required = 'catalog.can_mark_returned'
+    template_name = 'catalog/bookinstance_list_borrowed_all.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(status__exact='o').order_by('due_back')
